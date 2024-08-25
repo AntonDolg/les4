4b60e6b1421b4713924d562d37524bdbd8272d09
AntonDolg <modbus7@gmail.com>
2024-08-25 13:55:16 +0300

    change FormTests

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -1,5 +1,8 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -10,6 +13,7 @@ public class FormTests {
         open("https://github.com/selenide/selenide");
         $(".Layout-sidebar").$(byText("Contributors"))
                 .closest(".h4 mb-3").sibling(0).$$("li").first().hover();
+        $$(".Popover").findBy(visible).shouldHave(text("Andrei Solntsev"));
         sleep(5000);
     }
     
