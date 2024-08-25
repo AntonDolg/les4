78f8f11e1fb63b5139e094a17375980eb0213aa4
AntonDolg <modbus7@gmail.com>
2024-08-25 14:05:53 +0300

    change1 FormTests

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -10,9 +10,12 @@ public class FormTests {
 
     @Test
     void TestSelenide(){
+        //открыть репозиторий селенида на гитхабе
         open("https://github.com/selenide/selenide");
+        //навести мышь на первого учатника проекта
         $(".Layout-sidebar").$(byText("Contributors"))
                 .closest(".h4 mb-3").sibling(0).$$("li").first().hover();
+        //проверка: в всплывающем окне содержится Andrei Solntsev
         $$(".Popover").findBy(visible).shouldHave(text("Andrei Solntsev"));
         sleep(5000);
     }
